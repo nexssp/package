87f4f453d7d1841de863a0d0df3e1c6eecdae28d
Marcin Polak <mapoart@gmail.com>
2021-05-31 14:33:39 +0200

    Upgrade to the latest versions

--- src/config/spawnOptions.js
+++ src/config/spawnOptions.js
@@ -1,9 +1,11 @@
+const { getShell } = require('@nexssp/os')
+
 module.exports = (opts) => {
   const result = {}
   if (process.platform === 'win32') {
     Object.assign(result, { stdio: 'inherit' }, opts)
   } else {
-    Object.assign(result, { stdio: 'inherit', shell: process.shell }, opts)
+    Object.assign(result, { stdio: 'inherit', shell: getShell() }, opts)
   }
 
   return result
